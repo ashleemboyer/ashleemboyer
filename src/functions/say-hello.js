d2effb95f5bd61380dbf07f0aa8bd40b01badc54
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-08-25 11:43:37 -0400

    Updating say-hello

--- src/functions/say-hello.js
+++ src/functions/say-hello.js
@@ -1,3 +1,7 @@
+import { addSubscriber } from "../firebase";
+
 exports.handler = function(event, context, callback) {
-  callback(null, { body: "Hello!", statusCode: 200 });
+  const { name, email } = decodeURIComponent(event.queryStringParameters);
+  console.log(name, email);
+  callback(null, { body: "Hello!", statusCode: 200, something: "Hmmm" });
 };
