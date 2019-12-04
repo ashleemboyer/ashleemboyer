74fedb9cd6a08e0b6061538f4e6ec1c951205b3d
Ashlee Boyer <43934258+ashleemboyer@users.noreply.github.com>
2019-12-04 11:32:53 -0500

    Updating status of newsletter page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -26,94 +26,101 @@ const Newsletter = () => {
   return (
     <Layout>
       <div css={{ maxWidth: 700, margin: "0 auto" }}>
-        <h1>Sign up for my newsletter!</h1>
-        <p>
-          You'll receive all of my posts straight to your inbox. If I have
-          exciting news to share, such as upcoming talks or general life
-          updates, you'll be the first to know! And I promise: no spam! Spammy
-          newsletters suck.{" "}
-          <span role="img" aria-label="smiling face with smiling eyes emoji">
-            😊
-          </span>
-        </p>
+        <h1>Sorry about that.</h1>
+        <p>My newsletter is temporarily down. Nothing is broken, I'm just working on improving some of the automation! It should be up and running again at the beginning of 2020.</p>
 
-        <p>Both fields are required.</p>
+//
+//  TODO: uncomment when newsletter is complete
+//
+//         <h1>Sign up for my newsletter!</h1>
+//         <p>
+//           You'll receive all of my posts straight to your inbox. If I have
+//           exciting news to share, such as upcoming talks or general life
+//           updates, you'll be the first to know! And I promise: no spam! Spammy
+//           newsletters suck.{" "}
+//           <span role="img" aria-label="smiling face with smiling eyes emoji">
+//             😊
+//           </span>
+//         </p>
 
-        <section
-          css={{
-            border: "1px solid #C2185B",
-            padding: "24px",
-            maxWidth: "400px",
-            margin: "0 auto",
-            textAlign: "center",
-            marginTop: "32px",
-            borderRadius: "4px",
-          }}
-        >
-          <div css={{ margin: "16px 0" }}>
-            <label css={labelStyles} htmlFor="name-field">
-              Name
-            </label>
-            <input
-              css={inputStyles}
-              id="name-field"
-              type="text"
-              value={name}
-              onChange={({ target: { value } }) => {
-                setName(value);
-              }}
-            />
-          </div>
+//         <p>Both fields are required.</p>
 
-          <div>
-            <label css={labelStyles} htmlFor="email-field">
-              Email
-            </label>
-            <input
-              css={inputStyles}
-              id="email-field"
-              type="email"
-              value={email}
-              onChange={({ target: { value } }) => {
-                setEmail(value);
-              }}
-            />
-          </div>
+//         <section
+//           css={{
+//             border: "1px solid #C2185B",
+//             padding: "24px",
+//             maxWidth: "400px",
+//             margin: "0 auto",
+//             textAlign: "center",
+//             marginTop: "32px",
+//             borderRadius: "4px",
+//           }}
+//         >
+//           <div css={{ margin: "16px 0" }}>
+//             <label css={labelStyles} htmlFor="name-field">
+//               Name
+//             </label>
+//             <input
+//               css={inputStyles}
+//               id="name-field"
+//               type="text"
+//               value={name}
+//               onChange={({ target: { value } }) => {
+//                 setName(value);
+//               }}
+//             />
+//           </div>
 
-          <button
-            css={{
-              marginTop: "32px",
-              padding: "6px 12px",
-              border: "1px solid #C2185B",
-              borderRadius: "4px",
-              color: "#FFFFFF",
-              background: "#C2185B",
-              cursor: "pointer",
-              "&:hover": {
-                fontWeight: "bold",
-                "-webkit-box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
-                "-moz-box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
-                "box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
-              },
-            }}
-            onClick={() => {
-              if (email.indexOf("a") === -1) {
-                return;
-              }
+//           <div>
+//             <label css={labelStyles} htmlFor="email-field">
+//               Email
+//             </label>
+//             <input
+//               css={inputStyles}
+//               id="email-field"
+//               type="email"
+//               value={email}
+//               onChange={({ target: { value } }) => {
+//                 setEmail(value);
+//               }}
+//             />
+//           </div>
 
-              addSubscriber({ name, email }).then(result => {
-                if (result.success) {
-                  alert("Subscribe successful! Expect an email soon.");
-                  navigate("/");
-                } else {
-                  alert(`Could not subscribe. ${result.reason}`);
-                }
-              });
-            }}
-          >
-            Sign Me Up!
-          </button>
-        </section>
+//           <button
+//             css={{
+//               marginTop: "32px",
+//               padding: "6px 12px",
+//               border: "1px solid #C2185B",
+//               borderRadius: "4px",
+//               color: "#FFFFFF",
+//               background: "#C2185B",
+//               cursor: "pointer",
+//               "&:hover": {
+//                 fontWeight: "bold",
+//                 "-webkit-box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
+//                 "-moz-box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
+//                 "box-shadow": "1px 1px 4px 0px rgba(194,24,91,0.6)",
+//               },
+//             }}
+//             onClick={() => {
+//               if (email.indexOf("a") === -1) {
+//                 return;
+//               }
+
+//               addSubscriber({ name, email }).then(result => {
+//                 if (result.success) {
+//                   alert("Subscribe successful! Expect an email soon.");
+//                   navigate("/");
+//                 } else {
+//                   alert(`Could not subscribe. ${result.reason}`);
+//                 }
+//               });
+//             }}
+//           >
+//             Sign Me Up!
+//           </button>
+//         </section>
+
       </div>
     </Layout>
   );
