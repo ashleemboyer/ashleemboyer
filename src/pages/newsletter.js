f354e84f531a745e0a5dac522d78a216469a415b
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-05-15 22:42:09 -0400

    Fixing the newsletter form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -6,9 +6,9 @@ const Newsletter = () => (
   <Layout>
     <div css={{ maxWidth: 700, margin: '0 auto' }}>
       <div
-        class="ml-form-embed"
+        className="ml-form-embed"
         data-account="2065284:p5t2t6a3y0"
-        data-form="2094792:k1y2c8"
+        data-form="2098836:z0q3p8"
       ></div>
     </div>
   </Layout>
