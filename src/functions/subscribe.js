8a1b0a872cc94348b37dc9a7441038c2bd106349
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-05-16 19:35:04 -0400

    Newsletter signs people up

--- src/functions/subscribe.js
+++ src/functions/subscribe.js
@@ -1,26 +1,70 @@
 const fetch = require('node-fetch');
 
-exports.handler = async () => {
-  const data = await fetch('https://api.mailerlite.com/api/v2/subscribers', {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'Ashlee',
-      email: 'ashleemboyer.dev@gmail.com',
-      fields: {
-        tags: 'gatsby,blogging,accessibility',
-      },
-    }),
+exports.handler = async (event, context, callback) => {
+  const API_URL = 'https://api.mailerlite.com/api/v2';
+
+  const parsedBody = JSON.parse(event.body);
+  const name = parsedBody.name;
+  const email = parsedBody.email;
+  const tags = parsedBody.tags;
+
+  const existingSubscriber = await fetch(`${API_URL}/subscribers/${email}`, {
+    method: 'GET',
     headers: {
       'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY,
       'Content-Type': 'application/json',
     },
   })
-    .then(res => res.json())
+    .then(response => response.json())
     .then(result => result)
     .catch(err => console.error(err));
 
+  if (existingSubscriber.email) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: false,
+        message: `It looks like you've already subscribed with this email address: ${existingSubscriber.email}`,
+      }),
+    };
+  }
+
+  const newSubscriber = await fetch(
+    `${API_URL}/groups/${process.env.MAILERLITE_NEEDS_TO_CONFIRM_GROUP_ID}/subscribers`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        fields: {
+          tags,
+        },
+      }),
+      headers: {
+        'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY,
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then(response => response.json())
+    .then(result => result)
+    .catch(err => console.error(err));
+
+  if (newSubscriber.email) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        message: `Check your inbox (${newSubscriber.email}) for a confirmation email.`,
+      }),
+    };
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(data),
+    statusCode: 500,
+    body: JSON.stringify({
+      success: false,
+      message: 'There was a problem adding you to the system.',
+    }),
   };
 };
--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,18 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../components/layout';
 
-const Newsletter = () => (
-  <Layout>
-    <div css={{ maxWidth: 700, margin: '0 auto' }}>
-      <h2>The newsletter is coming soon. ❤️</h2>
-      {/* <div
-        className="ml-form-embed"
-        data-account="2065284:p5t2t6a3y0"
-        data-form="2098836:z0q3p8"
-      ></div> */}
-    </div>
-  </Layout>
-);
+const Loading = () => {
+  return <h1>Loading...</h1>;
+};
+
+const Form = ({ name, setName, email, setEmail, submitForm }) => {
+  return (
+    <form onSubmit={submitForm}>
+      <label>Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={e => {
+          e.preventDefault();
+          setName(e.target.value);
+        }}
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        value={email}
+        onChange={e => {
+          e.preventDefault();
+          setEmail(e.target.value);
+        }}
+      />
+      <button type="submit">Subscribe</button>
+    </form>
+  );
+};
+
+const Message = ({ message }) => {
+  return (
+    <>
+      <h1>
+        You're in!{' '}
+        <span role="img" aria-label="party popper emoji">
+          🎉
+        </span>
+      </h1>
+      <p>Thanks for signing up. {message}</p>
+    </>
+  );
+};
+
+const Error = ({ message }) => {
+  return (
+    <>
+      <h1>There might be a problem.</h1>
+      <p>{message}</p>
+      <p>
+        <a href="mailto:hello@ashleemboyer.com">Send Ashlee an email</a> and she
+        can probably help you out.
+      </p>
+    </>
+  );
+};
+
+const Newsletter = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const submitForm = async () => {
+    setLoading(true);
+
+    await fetch('/.netlify/functions/subscribe', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setLoading(false);
+        setMessage(data.message);
+
+        if (!data.success) {
+          setShowError(true);
+        }
+      })
+      .catch(err => {
+        setLoading(false);
+        setMessage(`There was an error in the request: ${err}.`);
+        setShowError(true);
+        console.error(err);
+      });
+  };
+
+  const getPageContents = () => {
+    if (loading) {
+      return <Loading />;
+    } else if (showError) {
+      return <Error message={message} />;
+    } else if (message) {
+      return <Message message={message} />;
+    } else {
+      return (
+        <Form
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          submitForm={submitForm}
+        />
+      );
+    }
+  };
+
+  return (
+    <Layout>
+      <div css={{ maxWidth: 700, margin: '0 auto' }}>{getPageContents()}</div>
+    </Layout>
+  );
+};
 
 export default Newsletter;
