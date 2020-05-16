4d552c105c1266ee04f6736b6a82f20c6059abb0
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-05-15 22:48:06 -0400

    Coming soon message on newsletter page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -5,11 +5,12 @@ import Layout from '../components/layout';
 const Newsletter = () => (
   <Layout>
     <div css={{ maxWidth: 700, margin: '0 auto' }}>
-      <div
+      <h2>The newsletter is coming soon. ❤️</h2>
+      {/* <div
         className="ml-form-embed"
         data-account="2065284:p5t2t6a3y0"
         data-form="2098836:z0q3p8"
-      ></div>
+      ></div> */}
     </div>
   </Layout>
 );
