9be97ad931ce5b26166d6fedaed2314f7b6b42cb
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-05-16 22:40:36 -0400

    Removing onSubmit prop from Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import styles from './Form.module.scss';
 
-const Form = ({ onSubmit, children }) => (
-  <form onSubmit={onSubmit} className={styles.Form}>
+const Form = ({ children }) => (
+  <form className={styles.Form}>
     <div>{children}</div>
   </form>
 );
