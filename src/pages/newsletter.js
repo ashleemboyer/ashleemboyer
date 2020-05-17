4f6b935258232153211d6e9f4f5f9461c371d68d
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-05-17 15:19:24 -0400

    Taking down newsletter for a little bit

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -33,109 +33,118 @@ const Error = ({ message }) => {
   );
 };
 
-const Newsletter = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState(false);
-  const [showError, setShowError] = useState(false);
+const Newsletter = () => (
+  <h1>
+    The newsletter is currently down but will be up and running again shortly.{' '}
+    <span role="img" aria-label="Red heart emoji.">
+      ❤️
+    </span>
+  </h1>
+);
 
-  const submitForm = async () => {
-    setLoading(true);
+// const Newsletter = () => {
+//   const [name, setName] = useState();
+//   const [email, setEmail] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const [message, setMessage] = useState(false);
+//   const [showError, setShowError] = useState(false);
 
-    await fetch('/.netlify/functions/subscribe', {
-      method: 'POST',
-      body: JSON.stringify({
-        name,
-        email,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        setLoading(false);
-        setMessage(data.message);
+//   const submitForm = async () => {
+//     setLoading(true);
 
-        if (!data.success) {
-          setShowError(true);
-        }
-      })
-      .catch(err => {
-        setLoading(false);
-        setMessage(`There was an error in the request: ${err}.`);
-        setShowError(true);
-        console.error(err);
-      });
-  };
+//     await fetch('/.netlify/functions/subscribe', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         name,
+//         email,
+//       }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         setLoading(false);
+//         setMessage(data.message);
 
-  const getPageContents = () => {
-    if (loading) {
-      return <Loading />;
-    } else if (showError) {
-      return <Error message={message} />;
-    } else if (message) {
-      return <Message message={message} />;
-    } else {
-      return (
-        <>
-          <h1 css>
-            Did you know I have a newsletter?{' '}
-            <span role="img" aria-label="Open mailbox with raised flag emoji.">
-              📬
-            </span>
-          </h1>
-          <p css={{ marginBottom: 32, lineHeight: '1.8rem' }}>
-            If you want to get notified when I publish new blog posts or make
-            major project announcements, this is the right place! All I need is
-            your name and email address.
-          </p>
-          <Form>
-            <h2 css={{ marginBottom: 32, fontSize: '1.8rem' }}>
-              Sign up here{' '}
-              <span
-                role="img"
-                aria-label="Index finger pointing downward emoji."
-              >
-                👇
-              </span>
-            </h2>
-            <label htmlFor="name">First name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={e => {
-                e.preventDefault();
-                setName(e.target.value);
-              }}
-            />
-            <label htmlFor="email">Email address</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={e => {
-                e.preventDefault();
-                setEmail(e.target.value);
-              }}
-            />
-            <button type="button" onClick={submitForm}>
-              Subscribe
-            </button>
-          </Form>
-        </>
-      );
-    }
-  };
+//         if (!data.success) {
+//           setShowError(true);
+//         }
+//       })
+//       .catch(err => {
+//         setLoading(false);
+//         setMessage(`There was an error in the request: ${err}.`);
+//         setShowError(true);
+//         console.error(err);
+//       });
+//   };
 
-  return (
-    <Layout>
-      <div css={{ textAlign: 'center', paddingTop: 32 }}>
-        {getPageContents()}
-      </div>
-    </Layout>
-  );
-};
+//   const getPageContents = () => {
+//     if (loading) {
+//       return <Loading />;
+//     } else if (showError) {
+//       return <Error message={message} />;
+//     } else if (message) {
+//       return <Message message={message} />;
+//     } else {
+//       return (
+//         <>
+//           <h1 css>
+//             Did you know I have a newsletter?{' '}
+//             <span role="img" aria-label="Open mailbox with raised flag emoji.">
+//               📬
+//             </span>
+//           </h1>
+//           <p css={{ marginBottom: 32, lineHeight: '1.8rem' }}>
+//             If you want to get notified when I publish new blog posts or make
+//             major project announcements, this is the right place! All I need is
+//             your name and email address.
+//           </p>
+//           <Form>
+//             <h2 css={{ marginBottom: 32, fontSize: '1.8rem' }}>
+//               Sign up here{' '}
+//               <span
+//                 role="img"
+//                 aria-label="Index finger pointing downward emoji."
+//               >
+//                 👇
+//               </span>
+//             </h2>
+//             <label htmlFor="name">First name</label>
+//             <input
+//               type="text"
+//               name="name"
+//               id="name"
+//               value={name}
+//               onChange={e => {
+//                 e.preventDefault();
+//                 setName(e.target.value);
+//               }}
+//             />
+//             <label htmlFor="email">Email address</label>
+//             <input
+//               type="email"
+//               name="email"
+//               id="email"
+//               value={email}
+//               onChange={e => {
+//                 e.preventDefault();
+//                 setEmail(e.target.value);
+//               }}
+//             />
+//             <button type="button" onClick={submitForm}>
+//               Subscribe
+//             </button>
+//           </Form>
+//         </>
+//       );
+//     }
+//   };
+
+//   return (
+//     <Layout>
+//       <div css={{ textAlign: 'center', paddingTop: 32 }}>
+//         {getPageContents()}
+//       </div>
+//     </Layout>
+//   );
+// };
 
 export default Newsletter;
