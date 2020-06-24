8378abd9b492555417fe000a3aba5fe5221d5cf7
Ashlee Boyer <ashlee@Iriss-MacBook-Pro.local>
2020-06-24 19:56:08 -0400

    Adding pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, graphql } from 'gatsby';
 
 import { rhythm } from '../utils/typography';
 import Layout from '../components/Layout/Layout';
 
-export default ({ data }) => (
-  <Layout>
-    <div>
-      <h1 css={{ fontSize: rhythm(2.4), marginBottom: rhythm(0.8) }}>
-        Journey of a Disabled Web Developer
-      </h1>
-      <h3 className="light" css={{ marginBottom: rhythm(2) }}>
-        She's written {data.allMarkdownRemark.totalCount} Posts
-      </h3>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
+const PAGE_SIZE = 5;
+
+export default ({ data }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const allPosts = data.allMarkdownRemark.edges;
+  const numPages = Math.ceil(data.allMarkdownRemark.totalCount / PAGE_SIZE) - 1;
+  const paginatedPosts = allPosts.slice(
+    PAGE_SIZE * currentPage + 1,
+    PAGE_SIZE * currentPage + 1 + PAGE_SIZE
+  );
+
+  return (
+    <Layout>
+      <div>
+        <h1 css={{ fontSize: rhythm(2.4), marginBottom: rhythm(0.8) }}>
+          Journey of a Disabled Web Developer
+        </h1>
+        <h3 className="light" css={{ marginBottom: rhythm(2) }}>
+          She's written {data.allMarkdownRemark.totalCount} Posts
+        </h3>
+        {paginatedPosts.map(({ node }) => (
+          <div
+            key={node.id}
+            css={{
+              // todo: selector for all but last?
+              marginBottom: rhythm(1.6),
+              border: '1px solid #bdbdbd',
+              borderRadius: '4px',
+              transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
+              ':hover': {
+                boxShadow:
+                  '0 10px 16px rgba(0,0,0,0.26), 0 6px 6px rgba(0,0,0,0.22)',
+              },
+              backgroundColor: '#fff',
+            }}
+          >
+            <Link
+              to={node.fields.slug}
+              css={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              {node.frontmatter.image && (
+                <img
+                  src={node.frontmatter.image}
+                  alt={node.frontmatter.alt}
+                  css={{
+                    borderRadius: '4px 4px 0 0',
+                    margin: 0,
+                    width: '100%',
+                  }}
+                />
+              )}
+              <div css={{ padding: '12px' }}>
+                <h2 css={{ marginBottom: rhythm(1 / 4) }}>
+                  {node.frontmatter.title}{' '}
+                  <span className="light">- {node.frontmatter.date}</span>
+                </h2>
+                <p css={{ margin: 0 }}>{node.excerpt}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
         <div
-          key={node.id}
           css={{
-            // todo: selector for all but last?
-            marginBottom: rhythm(1.6),
-            border: '1px solid #bdbdbd',
-            borderRadius: '4px',
-            transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
-            ':hover': {
-              boxShadow:
-                '0 10px 16px rgba(0,0,0,0.26), 0 6px 6px rgba(0,0,0,0.22)',
-            },
-            backgroundColor: '#fff',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          <Link
-            to={node.fields.slug}
-            css={{ textDecoration: 'none', color: 'inherit' }}
+          <button
+            onClick={() => {
+              if (currentPage > 0) {
+                setCurrentPage(currentPage - 1);
+              }
+            }}
           >
-            {node.frontmatter.image && (
-              <img
-                src={node.frontmatter.image}
-                alt={node.frontmatter.alt}
-                css={{
-                  borderRadius: '4px 4px 0 0',
-                  margin: 0,
-                  width: '100%',
-                }}
-              />
-            )}
-            <div css={{ padding: '12px' }}>
-              <h2 css={{ marginBottom: rhythm(1 / 4) }}>
-                {node.frontmatter.title}{' '}
-                <span className="light">- {node.frontmatter.date}</span>
-              </h2>
-              <p css={{ margin: 0 }}>{node.excerpt}</p>
-            </div>
-          </Link>
+            Prev
+          </button>
+          <p css={{ margin: '0 12px' }}>
+            {PAGE_SIZE * currentPage + 1} -{' '}
+            {PAGE_SIZE * currentPage + paginatedPosts.length} of{' '}
+            {data.allMarkdownRemark.totalCount}
+          </p>
+          <button
+            onClick={() => {
+              if (currentPage < numPages) {
+                setCurrentPage(currentPage + 1);
+              }
+            }}
+          >
+            Next
+          </button>
         </div>
-      ))}
-    </div>
-  </Layout>
-);
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query {
