75eb99bad89e15aa5330104d14a70a712f383ae5
Ashlee Boyer <ashlee@Iriss-MacBook-Pro.local>
2020-07-16 19:27:45 -0400

    Making the home page nicer

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,85 +18,95 @@ export default ({ data }) => {
 
   return (
     <Layout>
-      <div>
-        <h1 css={{ fontSize: rhythm(2.4), marginBottom: rhythm(0.8) }}>
-          Journey of a Disabled Web Developer
-        </h1>
-        <h3 className="light" css={{ marginBottom: rhythm(2) }}>
-          She's written {data.allMarkdownRemark.totalCount} Posts
-        </h3>
-        {paginatedPosts.map(({ node }) => (
-          <div
-            key={node.id}
-            css={{
-              // todo: selector for all but last?
-              marginBottom: rhythm(1.6),
-              border: '1px solid #bdbdbd',
-              borderRadius: '4px',
-              transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
-              ':hover': {
-                boxShadow:
-                  '0 10px 16px rgba(0,0,0,0.26), 0 6px 6px rgba(0,0,0,0.22)',
-              },
-              backgroundColor: '#fff',
-            }}
-          >
-            <Link
-              to={node.fields.slug}
-              css={{ textDecoration: 'none', color: 'inherit' }}
-            >
-              {node.frontmatter.image && (
-                <img
-                  src={node.frontmatter.image}
-                  alt={node.frontmatter.alt}
-                  css={{
-                    borderRadius: '4px 4px 0 0',
-                    margin: 0,
-                    width: '100%',
-                  }}
-                />
-              )}
-              <div css={{ padding: '12px' }}>
-                <h2 css={{ marginBottom: rhythm(1 / 4) }}>
-                  {node.frontmatter.title}{' '}
-                  <span className="light">- {node.frontmatter.date}</span>
-                </h2>
-                <p css={{ margin: 0 }}>{node.excerpt}</p>
-              </div>
-            </Link>
-          </div>
-        ))}
+      <h1
+        css={{
+          fontSize: rhythm(2.4),
+          marginBottom: '16px',
+        }}
+      >
+        Journey of a Disabled Web Developer
+      </h1>
+      <h3 className="light" css={{ marginBottom: '48px' }}>
+        She's written {data.allMarkdownRemark.totalCount} Posts
+      </h3>
+      {paginatedPosts.map(({ node }) => (
         <div
+          key={node.id}
           css={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            marginBottom: rhythm(1.6),
           }}
         >
-          <button
-            onClick={() => {
-              if (currentPage > 0) {
-                setCurrentPage(currentPage - 1);
-              }
-            }}
-          >
-            Prev
-          </button>
-          <p css={{ margin: '0 12px' }}>
-            {PAGE_SIZE * currentPage + 1} -{' '}
-            {PAGE_SIZE * currentPage + paginatedPosts.length} of{' '}
-            {data.allMarkdownRemark.totalCount}
-          </p>
-          <button
-            onClick={() => {
-              if (currentPage < numPages) {
-                setCurrentPage(currentPage + 1);
-              }
-            }}
-          >
-            Next
-          </button>
+          <div id={node.id}>
+            <h2 css={{ fontSize: '2rem', marginBottom: 12 }}>
+              {node.frontmatter.title}
+            </h2>
+            <p css={{ margin: 0, marginBottom: '12px' }}>{node.excerpt}</p>
+            <Link
+              to={node.fields.slug}
+              css={{
+                color: '#C2185B',
+              }}
+            >
+              Keep Reading
+            </Link>
+          </div>
+          <hr />
         </div>
+      ))}
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {currentPage > 0 && (
+          <button
+            css={{
+              padding: 0,
+              border: 0,
+              backgroundColor: 'transparent',
+              color: '#c2185b',
+              textDecoration: 'underline',
+            }}
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+              const firstPost = document.getElementById(
+                paginatedPosts[0].node.id
+              );
+              const boundingClientRect = firstPost.getBoundingClientRect();
+              window.scrollBy(0, boundingClientRect.y - 20);
+            }}
+          >
+            Newer Posts
+          </button>
+        )}
+        <p css={{ margin: '0 12px', textAlign: 'center' }}>
+          {PAGE_SIZE * currentPage + 1} -{' '}
+          {PAGE_SIZE * currentPage + paginatedPosts.length} of{' '}
+          {data.allMarkdownRemark.totalCount}
+        </p>
+        {currentPage < numPages && (
+          <button
+            css={{
+              padding: 0,
+              border: 0,
+              backgroundColor: 'transparent',
+              color: '#c2185b',
+              textDecoration: 'underline',
+            }}
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+              const firstPost = document.getElementById(
+                paginatedPosts[0].node.id
+              );
+              const boundingClientRect = firstPost.getBoundingClientRect();
+              window.scrollBy(0, boundingClientRect.y - 20);
+            }}
+          >
+            Older Posts
+          </button>
+        )}
       </div>
     </Layout>
   );
