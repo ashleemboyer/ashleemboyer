275fd205ae830cd8404e60a84d62c83175713fce
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-11-05 20:30:29 -0500

    Passing title to layout component

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 import { Layout } from '../components';
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout title="Not Found">
     <h1>You might be lost.</h1>
     <p>That page doesn't exist here!</p>
   </Layout>
