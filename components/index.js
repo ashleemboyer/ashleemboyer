2c66943b89488b9020e39d26acc2d6630a300b78
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-12 23:10:49 -0500

    Adding icon component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 export { Button } from './Button';
 export { CodeBlock } from './CodeBlock';
 export { Form } from './Form';
+export { Icon } from './Icon';
 export { Layout } from './Layout';
 export { PostList } from './PostList';
