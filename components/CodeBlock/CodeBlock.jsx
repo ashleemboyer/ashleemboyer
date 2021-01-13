f6757c2ccdc61b6a1cba963fd9e85d059f5592ea
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-12 23:12:12 -0500

    Adding actual content to CodeBlock

--- components/CodeBlock/CodeBlock.jsx
+++ components/CodeBlock/CodeBlock.jsx
@@ -1,24 +1,32 @@
+import { Icon } from '../';
 import styles from './CodeBlock.module.scss';
 
-const CodeBlock = ({ lines, clipboardContent }) => (
+const CodeBlock = ({ language, lines, clipboardContent }) => (
   <div className={styles.CodeBlock}>
-    <button
-      onClick={() => {
-        navigator.clipboard.writeText(clipboardContent.join('\n'));
-      }}
-    >
-      copy
-    </button>
-    <div>
-      {lines.map((_, index) => (
-        <div className="line-number">{index + 1}</div>
-      ))}
+    <div className={styles['CodeBlock-toolbar']}>
+      <span>{language}</span>
+      <span>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(clipboardContent.join('\n'));
+          }}
+        >
+          <Icon name="clipboard" />
+        </button>
+      </span>
     </div>
-    <pre>
-      {lines.slice(0, lines.length - 1).map((thing) => (
-        <div className="line-of-code">{thing}</div>
-      ))}
-    </pre>
+    <div className={styles['CodeBlock-content']}>
+      <div className={styles['CodeBlock-content-lineNumbers']}>
+        {lines.map((_, index) => (
+          <div>{index + 1}</div>
+        ))}
+      </div>
+      <pre>
+        {lines.map((thing) => (
+          <div>{thing}</div>
+        ))}
+      </pre>
+    </div>
   </div>
 );
 
