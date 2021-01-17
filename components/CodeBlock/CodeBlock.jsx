9ce0ac170fef07705e724a394c1c8698ddd80c63
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-01-17 11:52:14 -0500

    Showing clipboard-check after copy button is clicked

--- components/CodeBlock/CodeBlock.jsx
+++ components/CodeBlock/CodeBlock.jsx
@@ -1,33 +1,45 @@
+import { useState } from 'react';
 import { Icon } from '../';
 import styles from './CodeBlock.module.scss';
 
-const CodeBlock = ({ language, lines, clipboardContent }) => (
-  <div className={styles.CodeBlock}>
-    <div className={styles['CodeBlock-toolbar']}>
-      <span>{language}</span>
-      <span>
-        <button
-          onClick={() => {
-            navigator.clipboard.writeText(clipboardContent.join('\n'));
-          }}
-        >
-          <Icon name="clipboard" />
-        </button>
-      </span>
-    </div>
-    <div className={styles['CodeBlock-content']}>
-      <div className={styles['CodeBlock-content-lineNumbers']}>
-        {lines.map((_, index) => (
-          <div>{index + 1}</div>
-        ))}
+const CodeBlock = ({ language, lines, clipboardContent }) => {
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <div className={styles.CodeBlock}>
+      <div className={styles['CodeBlock-toolbar']}>
+        <span>{language}</span>
+        <span>
+          <button
+            onClick={() => {
+              if (!copied) {
+                navigator.clipboard.writeText(clipboardContent.join('\n'));
+                setCopied(true);
+
+                setTimeout(() => {
+                  setCopied(false);
+                }, 5000);
+              }
+            }}
+          >
+            <Icon name={copied ? 'clipboard-check' : 'clipboard'} />
+          </button>
+        </span>
       </div>
-      <pre>
-        {lines.map((thing) => (
-          <div>{thing}</div>
-        ))}
-      </pre>
+      <div className={styles['CodeBlock-content']}>
+        <div className={styles['CodeBlock-content-lineNumbers']}>
+          {lines.map((_, index) => (
+            <div>{index + 1}</div>
+          ))}
+        </div>
+        <pre>
+          {lines.map((line) => (
+            <div>{line}</div>
+          ))}
+        </pre>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CodeBlock;
