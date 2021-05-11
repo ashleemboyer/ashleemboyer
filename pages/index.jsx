a6bfff5ae30b16ee689ee90a5ec117f715a021a2
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-05-10 21:19:21 -0400

    Returning posts for both site and feed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+const Feed = require('feed').Feed;
+import fs from 'fs';
 import { getAllPosts } from '../lib/api';
 import { Layout, PostList } from '../components';
 
@@ -15,9 +17,9 @@ const HomePage = ({ posts }) => (
 );
 
 export async function getStaticProps() {
-  let posts = getAllPosts();
+  let { forSite, forRSSFeed } = getAllPosts();
 
-  posts = posts.sort((a, b) => {
+  const posts = forSite.sort((a, b) => {
     if (a.meta.date < b.meta.date) {
       return 1;
     }
@@ -25,7 +27,49 @@ export async function getStaticProps() {
     return -1;
   });
 
-  return { props: { posts: posts } };
+  const baseUrl = 'https://ashleemboyer.com';
+  const date = new Date();
+  const author = {
+    name: 'Ashlee Boyer',
+    email: 'hello@ashleemboyer.com',
+    link: 'https://twitter.com',
+  };
+  const feed = new Feed({
+    title: 'Ashlee M Boyer',
+    description:
+      "You can find me talking about issues surrounding Disability, Accessibility, and Mental Health on Twitter, or you can find me regularly live-knitting or live-coding on Twitch. I'm @AshleeMBoyer on all the platforms I use.",
+    id: baseUrl,
+    link: baseUrl,
+    language: 'en',
+    updated: date,
+    feedLinks: {
+      rss2: `${baseUrl}/feed.xml`,
+    },
+    author,
+  });
+
+  forRSSFeed.forEach((post) => {
+    const {
+      content,
+      fileName,
+      meta: { date, title },
+    } = post;
+    const url = `${baseUrl}/${fileName}`;
+
+    feed.addItem({
+      title,
+      id: url,
+      link: url,
+      description: content,
+      author: [author],
+      contributor: [author],
+      date: new Date(date),
+    });
+  });
+
+  fs.writeFileSync('./public/rss.xml', feed.rss2());
+
+  return { props: { posts } };
 }
 
 export default HomePage;
