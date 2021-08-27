a4cc7f0f32d5751fc927b338b6c60448881ab236
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-08-27 17:24:00 -0400

    Making import nicer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 
-import '../stylesheets/global.scss';
-import '../stylesheets/typography.scss';
+import 'stylesheets/global.scss';
+import 'stylesheets/typography.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
